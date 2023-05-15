c308680bdee94a7fb493e6375b2760f7671a0e53
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-05-15 23:57:01 +0200

    add react router 5.3.3

--- src/core/App.js
+++ src/core/App.js
@@ -1,3 +1,4 @@
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import { Header } from "../common/Header";
 import { BurgerMenu } from "../common/Header/BurgerMenu";
 import { GlobalStyle } from "./GlobalStyle";
@@ -6,8 +7,21 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <BurgerMenu/>
+      <HashRouter>
+            <Header />
+            <BurgerMenu/>
+            <Switch>
+              <Route path="/o-nas"></Route>
+              <Route path="/uslugi"></Route>
+              <Route path="/minikoparka"></Route>
+              <Route path="/realizacje"></Route>
+              <Route path="/lokalizacja"></Route>
+              <Route path="/kontakt"></Route>
+              <Route path="/">
+                <Redirect to="/o-nas" /></Route>
+            </Switch>
+          </HashRouter >
+      
     </>
   );
 }
