1a1ada8ae81fd2232d673afa6a094f7d78ba4ddc
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-05-17 00:31:38 +0200

    add responsive to aboutUs

--- src/features/AboutUs/index.js
+++ src/features/AboutUs/index.js
@@ -3,20 +3,22 @@ import owner from "./RadosławUrbaniak.jpg"
 
 export const AboutUs = () => {
   return (
-  <>
-  <Image src={owner} alt="Radosław Urbaniak photo"/>
-  <StyledArticle>
-    <Title><strong>Usługi minikoparką</strong> - kilka słów o nas</Title>
-  <Subtitle>Główny operator:</Subtitle>
-  <Content>
-    Radosław Urbaniak- właściciel firmy, <strong>oprator minikoparki</strong>, posiada 11 lat udokumentowanego doświadczenia.
-    Jako założyciel PHU Urbaniak z najwyższą dbałością dogląda i realizuje zarówno plan prac, 
-    detale, bezpieczeństwo oraz kontakt z klientem.
-    Cechują go profesjonalizm, uczciwość, rzetelność i najwyższa jakość świadczonych usług. 
-    Zawsze znajdzie z Państwem wspólny język, odpowie na każde pytanie, doradzi i zaplanuje pracę oraz zorganizuje wszystkie potrzebne elementy.
-    Często również własnoręcznie wykonuje zlecenie co skutkuje najwyższą jakością usługi i oczywiście Państwa komfortem.
-  </Content>
-  </StyledArticle>
-  </>
+    <>
+
+      <StyledArticle>
+        <Title><strong>Usługi minikoparką</strong> - kilka słów o nas</Title>
+        <Subtitle>Główny operator:</Subtitle>
+        <Content>
+          <Image src={owner} alt="Radosław Urbaniak photo" />
+          <div>
+            Radosław Urbaniak- właściciel firmy, <strong>oprator minikoparki</strong>, posiada 11 lat udokumentowanego doświadczenia.            Jako założyciel PHU Urbaniak z najwyższą dbałością dogląda i realizuje zarówno plan prac,
+            detale, bezpieczeństwo oraz kontakt z klientem.
+            Cechują go profesjonalizm, uczciwość, rzetelność i najwyższa jakość świadczonych usług.
+            Zawsze znajdzie z Państwem wspólny język, odpowie na każde pytanie, doradzi i zaplanuje pracę oraz zorganizuje wszystkie potrzebne elementy.
+            Często również własnoręcznie wykonuje zlecenie co skutkuje najwyższą jakością usługi i oczywiście Państwa komfortem.
+          </div>
+        </Content>
+      </StyledArticle>
+    </>
   );
 };
--- src/features/AboutUs/styled.js
+++ src/features/AboutUs/styled.js
@@ -4,22 +4,40 @@ export const Image = styled.img`
 border-radius: 5px;
 height: 320px;
 margin-top: 16px;
+
+@media (max-width: 767px){
+  width: 40%;
+  height: 40%;
+}
 `
 
 export const StyledArticle = styled.article`
-  width: 100%;
+  width: calc(100%-32px);
   display: flex;
   flex-direction: column;
+  margin: 0 16px;
 `
 
 export const Title = styled.h2`
 font-size: 38px;
 text-align: center;
+
+@media (max-width: 767px) {
+  font-size: 22px;
+}
 `
 export const Subtitle = styled.h3`
-
+margin-left: 16px;
 `
 
 export const Content = styled.div`
+display: flex;
+gap: 16px;
+align-items: center;
 
+@media (max-width: 767px) {
+  flex-direction: column;
+  margin: 0 16px 16px 16px;
+flex-wrap:wrap
+}
 `
