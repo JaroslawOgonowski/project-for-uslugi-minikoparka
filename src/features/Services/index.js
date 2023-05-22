7114730bd684b8dbcf5a8fdc1acb29589b058217
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-05-22 22:44:59 +0200

    add services image

--- src/features/Services/index.js
+++ src/features/Services/index.js
@@ -1,32 +1,37 @@
-import { MainItem, ServicesDetailsList, StyledServices, Title } from "./styled"
+import { Box, Image, MainItem, ServicesDetailsList, ServicesList, StyledServices, Title } from "./styled"
 
 export const Services = () => {
   return (
-    <StyledServices>
-      <Title>Nasze usługi</Title>
-      <ul>
+    <>
 
-        <MainItem>montaż instalacji wodnych i systemów kanalizacyjnych</MainItem>
-        <MainItem>przyłącza wodociągowe</MainItem>
-        <MainItem>odkrywki starszych instalacji i fundamentów</MainItem>
-        <MainItem>wyrównanie i podnoszenie terenu</MainItem>
-        <MainItem>prace kompleksowe przy rozpoczęciu budowy</MainItem>
-        <MainItem>budowa podjazdów garażowych i wzjazdów na teren działki</MainItem>
-        <MainItem>przygotowanie terenu pod ogród</MainItem>
-        <MainItem>
-          Wykopy pod:
-          <ServicesDetailsList>
-            <li>fundamenty</li>
-            <li>instalacje: wodne, gazowe, elektryczne</li>
-            <li>małą zabudowę: garaże, ogrodzenia, tarasy</li>
-            <li>domowe oczyszczalnie i szambo</li>
-            <li>systemy nawodnień i drenaż działki</li>
-            <li>oczka wodne, stawy</li>
-            <li>podjazdy, wyjazdy, ścieżki</li>
-          </ServicesDetailsList>
-        </MainItem>
-        <MainItem>wszelkie wykopy do głębokości 2.6m</MainItem>
-      </ul>
-    </StyledServices>
+      <StyledServices>
+        <Title>Nasze usługi</Title>
+        <Box>
+          <ServicesList>
+            <MainItem>montaż instalacji wodnych i systemów kanalizacyjnych</MainItem>
+            <MainItem>przyłącza wodociągowe</MainItem>
+            <MainItem>odkrywki starszych instalacji i fundamentów</MainItem>
+            <MainItem>wyrównanie i podnoszenie terenu</MainItem>
+            <MainItem>prace kompleksowe przy rozpoczęciu budowy</MainItem>
+            <MainItem>budowa podjazdów garażowych i wzjazdów na teren działki</MainItem>
+            <MainItem>przygotowanie terenu pod ogród</MainItem>
+            <MainItem>
+              Wykopy pod:
+              <ServicesDetailsList>
+                <li>fundamenty</li>
+                <li>instalacje: wodne, gazowe, elektryczne</li>
+                <li>małą zabudowę: garaże, ogrodzenia, tarasy</li>
+                <li>domowe oczyszczalnie i szambo</li>
+                <li>systemy nawodnień i drenaż działki</li>
+                <li>oczka wodne, stawy</li>
+                <li>podjazdy, wyjazdy, ścieżki</li>
+              </ServicesDetailsList>
+            </MainItem>
+            <MainItem>wszelkie wykopy do głębokości 2.6m</MainItem>
+          </ServicesList>
+          <Image />
+        </Box>
+      </StyledServices>
+    </>
   )
 } 
--- src/features/Services/styled.js
+++ src/features/Services/styled.js
@@ -19,15 +19,28 @@ animation: servicesAnimation 1s ease normal forwards;
 	}
 }
 `
+export const Box = styled.div`
+width: 100%;
+display: flex;
+gap: 10vw;
+`
 
 export const Title = styled.h2`
   font-size: 38px;
   text-align: center;
+  width: 100%;
 
   @media (max-width: 767px) {
     font-size: 22px;
   }
 `
+export const ServicesList = styled.ul`
+list-style: none;
+  li::before {
+    content: "👷‍♂️";
+    margin-right: 0.5em;
+  }
+`;
 
 export const ServicesDetailsList = styled.ul`
 list-style: none;
@@ -39,4 +52,28 @@ list-style: none;
 
 export const MainItem = styled.li`
 margin-bottom: 16px;
+`
+
+export const Image = styled.div`
+width: 40%;
+aspect-ratio: 3/3.5;
+border-radius: 5px;
+background-image: url(https://scontent.fwaw3-2.fna.fbcdn.net/v/t39.30808-6/311600690_554118770046708_3289223615055676817_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=22Ku-LbEQ6gAX-dwh7J&_nc_ht=scontent.fwaw3-2.fna&oh=00_AfBs0rUXIbjAPn9rYVlbjAV23ziHkAIMKvDeput6vn4quw&oe=64706F9D);
+background-repeat: no-repeat;
+background-size: cover;
+
+animation-name: backgroundAnimation;
+  animation-duration: 30s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+  @keyframes backgroundAnimation {
+    0%, 25%{background-image: url(https://scontent.fwaw3-2.fna.fbcdn.net/v/t39.30808-6/311600690_554118770046708_3289223615055676817_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=22Ku-LbEQ6gAX-dwh7J&_nc_ht=scontent.fwaw3-2.fna&oh=00_AfBs0rUXIbjAPn9rYVlbjAV23ziHkAIMKvDeput6vn4quw&oe=64706F9D);    
+    background-position-x: 50%;}
+    30%, 60%{background-image: url(https://scontent.fwaw3-2.fna.fbcdn.net/v/t39.30808-6/316417724_581764297282155_3269857153402847636_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=Vtyj5rV4RB8AX8cFYT-&_nc_ht=scontent.fwaw3-2.fna&oh=00_AfC-moY6rZNpNduLmE0KaevA2E7JNb3NyPm-uBxQKmlxzg&oe=64709124);
+    background-position-x: 30%;}
+    65%, 90% {background-image: url(https://scontent.fwaw3-1.fna.fbcdn.net/v/t39.30808-6/336482438_196312079818307_2280192020488992182_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=yZEuK61jd3AAX-0AFgg&_nc_ht=scontent.fwaw3-1.fna&oh=00_AfD7lrZXj-HKXU_8hLqgNdZ0reLqop3HbPc1TOUyu1GGww&oe=6470096A);
+    background-position-x: 90%;}
+    95%, 100%{background-image: url(https://scontent.fwaw3-2.fna.fbcdn.net/v/t39.30808-6/311600690_554118770046708_3289223615055676817_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=22Ku-LbEQ6gAX-dwh7J&_nc_ht=scontent.fwaw3-2.fna&oh=00_AfBs0rUXIbjAPn9rYVlbjAV23ziHkAIMKvDeput6vn4quw&oe=64706F9D);
+    background-position-x:50%;}   
+  }
 `
