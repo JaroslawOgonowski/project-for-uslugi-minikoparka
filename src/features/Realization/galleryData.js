9d2c597d8342f853324eb3ba69eca53e80a8dab5
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-06-05 23:54:18 +0200

    fixed gallery 7-9

--- src/features/Realization/galleryData.js
+++ src/features/Realization/galleryData.js
@@ -17,6 +17,15 @@ import g5p3 from "./images/g5p3.jpg"
 import g6p1 from "./images/g6p1.jpg"
 import g6p2 from "./images/g6p2.jpg"
 import g6p3 from "./images/g6p3.jpg"
+import g7p1 from "./images/g7p1.jpg"
+import g7p2 from "./images/g7p2.jpg"
+import g7p3 from "./images/g7p3.jpg"
+import g8p1 from "./images/g8p1.jpg"
+import g8p2 from "./images/g8p2.jpg"
+import g8p3 from "./images/g8p3.jpg"
+import g9p1 from "./images/g9p1.jpg"
+import g9p2 from "./images/g9p2.jpg"
+import g9p3 from "./images/g9p3.jpg"
 export const galleryData =
   [
     {
@@ -44,12 +53,12 @@ export const galleryData =
       id: nanoid(),
       firstURL: g4p1,
       secondURL: g4p2,
-      thirdURL:  g4p3,
+      thirdURL: g4p3,
       content: "Budujemy przyłącza wodno-kanalizacyjne!"
     },
     {
       id: nanoid(),
-      firstURL:  g5p1,
+      firstURL: g5p1,
       secondURL: g5p2,
       thirdURL: g5p3,
       content: "Przyłącze kanalizacyjne z odprowadzeniem do szczelnego zbiornika"
@@ -63,23 +72,23 @@ export const galleryData =
     },
     {
       id: nanoid(),
-      firstURL: "https://scontent.fwaw3-2.fna.fbcdn.net/v/t39.30808-6/279222123_532566158422387_3834304354406565355_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=_QsVc0Nue1cAX8zbMUt&_nc_ht=scontent.fwaw3-2.fna&oh=00_AfBWdtjjNbr4rdlWOHNra_pd8k_HyLw4CEPn41jMgdosZQ&oe=64776DFD",
-      secondURL: "https://scontent.fwaw3-2.fna.fbcdn.net/v/t39.30808-6/279253270_532566305089039_401808905247205343_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=JH2qv8Dc3HMAX80Sz4X&_nc_ht=scontent.fwaw3-2.fna&oh=00_AfA5d_Fl9XdxPDj9eTUGtJ1fg3nMbhL5CR2HAFdGl7Yaig&oe=647884AD",
-      thirdURL: "https://scontent.fwaw3-1.fna.fbcdn.net/v/t39.30808-6/279241166_532566358422367_5670521076611961341_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=JEltYRJ-hk4AX-Gj_ut&_nc_ht=scontent.fwaw3-1.fna&oh=00_AfBoAUtLqQSHMVdFrZFrOoMf80yEHNmoscIzHLz9xNgAKg&oe=6477ABDC",
+      firstURL: g7p1,
+      secondURL: g7p2,
+      thirdURL: g7p3,
       content: "Prace kompleksowe przy rozpoczęciu budowy"
     },
     {
       id: nanoid(),
-      firstURL: "https://scontent.fwaw3-2.fna.fbcdn.net/v/t39.30808-6/277159879_510604373951899_7177339088011984096_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=SbFWlRQ3098AX8w3Jae&_nc_ht=scontent.fwaw3-2.fna&oh=00_AfAK-PGNG9ekTepvEEVs9M7ccc1DPsLvaTtH24cK6wtM2w&oe=6477E5CE",
-      secondURL: "https://scontent.fwaw3-2.fna.fbcdn.net/v/t39.30808-6/276266537_510603667285303_237092497879680740_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=Ml9rBG-hap8AX9ZfoPJ&_nc_ht=scontent.fwaw3-2.fna&oh=00_AfDzW7qkx3HfkTFZ11MuZ-Z78hxZGABOEhKGiu2nCdNM-w&oe=6477BD53",
-      thirdURL: "https://scontent.fwaw3-1.fna.fbcdn.net/v/t39.30808-6/277161302_510604317285238_1560363241956909232_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=fXS8IMhFlrIAX98An_Q&_nc_ht=scontent.fwaw3-1.fna&oh=00_AfDBe8a-6sFjcEb8aQfPuH7lQA9h6qAssep_jmn5DYqpHw&oe=64783AAC",
+      firstURL: g8p1,
+      secondURL: g8p2,
+      thirdURL: g8p3,
       content: "Budowa podjazdu"
     },
     {
       id: nanoid(),
-      firstURL: "https://scontent.fwaw3-2.fna.fbcdn.net/v/t39.30808-6/274099952_488438279501842_3700059608082108559_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=ljQptClLEGkAX89BIyh&_nc_ht=scontent.fwaw3-2.fna&oh=00_AfCLyCyrU0I8WIM8N91DdT3ULguqAeNfX3UdOsyxdyBwWQ&oe=6477B4E5",
-      secondURL: "https://scontent.fwaw3-1.fna.fbcdn.net/v/t39.30808-6/274092754_488438352835168_3667788182365956252_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=9iX8s_aJSDQAX-3JXtc&_nc_ht=scontent.fwaw3-1.fna&oh=00_AfC3RSqSpSxKpz5TyM3KePEfvTvb-hUACxWBLKjEyp5XpQ&oe=6477C1BE",
-      thirdURL: "https://scontent.fwaw3-1.fna.fbcdn.net/v/t39.30808-6/274092055_488438426168494_448910910979303178_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=B4iJHNvn_48AX_Ny0OT&_nc_ht=scontent.fwaw3-1.fna&oh=00_AfAj-12nBwfyjoayuxyGETeWTEqaZzAYM0RG0pcyYBmSFw&oe=647758F8",
+      firstURL: g9p1,
+      secondURL: g9p2,
+      thirdURL: g9p3,
       content: "Instalacja gazowa i przygotowanie terenu pod ogród"
     },
   ]
